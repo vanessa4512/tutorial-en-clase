662722ad7366cd489583e666676527348aa8a2f3
vanessa4512 <vanesita1208@gmail.com>
2026-02-18 14:33:28 -0600

    todo arreglado

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,12 +6,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float       moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2     moveInput;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
 
@@ -21,7 +23,19 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void Move(InputAction.CallbackContext context) {
+
+        animator.SetBool( "IsWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool( "IsWalking", false);
+            animator.SetFloat( "LastInputX", moveInput.x);
+            animator.SetFloat( "LastInputY", moveInput.y);
+        }
         moveInput = context.ReadValue<Vector2>();
+
+        animator.SetFloat( "InputX", moveInput.x);
+        animator.SetFloat( "InputY", moveInput.y);
     }
 
 }
